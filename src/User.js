1aabcd83402adddca3afca632b80bcd900caee9c
Zeinab Roumieh <zeinabroumieh@outlook.com>
2024-05-10 11:56:43 +0100

    user class

--- src/User.js
+++ src/User.js
@@ -1,5 +1,23 @@
 class User {
   // User code here
+  #password
+  constructor(username,password,age){
+    this.username = username;
+    this.#password = password
+    this.age = age
+    this.loggedIn = false
+  }
+
+  login(password){
+    if (this.#password!==password){
+      throw new Error("Incorrect password")
+    }
+    this.loggedIn = true
+  }
+
+  logout(){
+    this.loggedIn = false
+  }
 }
 
 module.exports = User
