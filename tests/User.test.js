2ccc4bb176786bf43f168a0e1434d0b22e1486dd
Zeinab Roumieh <zeinabroumieh@outlook.com>
2024-05-10 11:54:09 +0100

    user class tests

--- tests/User.test.js
+++ tests/User.test.js
@@ -5,14 +5,35 @@ const user = new User('Joe Bloggs', 'test123', 21)
 // User tests here
 describe('User property tests', () => {
   // test username
-  test('username should be a string', () => {
+  test('username should be a string and correctly assigned', () => {
     expect(typeof user.username).toBe('string')
+    expect(user.username).toBe("Joe Bloggs")
   })
   // test password
-
+  test("password is a string",()=>{
+    expect(typeof user.password).toBe('string')
+  })
   // test age
+  test("age is correctly assigned and number",()=>{
+    expect(typeof user.age).toBe("number")
+    expect(user.age).toBe(21)
+  })
 })
 
-// test login
+describe("User method tests", ()=>{
+  // test logout
+  test("user can log out",()=>{
+    user.logout()
+    expect(user.loggedIn).toBe(false)
+  })
 
-// test logout
+  // test login
+  test("can log in with correct password",()=>{
+    user.login('test123')
+    expect(user.loggedIn).toBe(true)
+  })
+
+  test("user cannot login with incorrect password",()=>{
+    expect(()=>user.login('hi')).toThrow("Incorrect password")
+  })
+})
